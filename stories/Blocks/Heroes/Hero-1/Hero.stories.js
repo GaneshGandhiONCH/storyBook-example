5728919e61fe27146db115996df53b4a90888eac
vadim <frghfqv86>
2021-12-19 22:06:36 +0200

    add controls for hero 1 delete layout, will use grid

--- stories/Blocks/Heroes/Hero-1/Hero.stories.js
+++ stories/Blocks/Heroes/Hero-1/Hero.stories.js
@@ -2,21 +2,24 @@ import hero_1 from './hero-images.svg'
 
 export default {
   title: 'Blocks/Heroes/Hero-1',
+  argTypes: {
+    title: { control: 'text' },
+    text: { control: 'text' },
+    buttonText: { control: 'text' },
+  }
 };
 
-const Template = () => {
+const Template = ({title, text, buttonText}) => {
   return `<section class="hero-1 v-container">
-    <div class="hero-1__content v-col-6">
-      <h1 class="hero-1__title title title-h3">Want anything to be easy with LaslesVPN.</h1>
-      <p class="hero-1__text">
-        Provide a network for all your needs with ease and fun using LaslesVPN discover interesting features from us.
-      </p>
+    <div class="hero-1__content">
+      <h1 class="hero-1__title title title-h3">${title}</h1>
+      <p class="hero-1__text">${text}</p>
       <img src="${hero_1}"
         alt="Some peoples with phone"
-        class="hero-1__img hero-1__img--mobile v-col-6"
+        class="hero-1__img hero-1__img--mobile"
         loading="lazy"
       >
-      <a href="#!" class="hero-1__button button-primary button-primary--medium">Get Started</a>
+      <a href="#!" class="hero-1__button button-primary button-primary--medium">${buttonText}</a>
     </div>
     <img src="${hero_1}"
       alt="Some peoples with phone"
@@ -27,3 +30,8 @@ const Template = () => {
 };
 
 export const Hero1 = Template.bind({});
+Hero1.args = {
+  title: 'Want anything to be easy with LaslesVPN.',
+  text: 'Provide a network for all your needs with ease and fun using LaslesVPN discover interesting features from us.',
+  buttonText: 'Get Started'
+};
