6018352b47dfdf0a33567bc76d20f025e54d10fb
vadim <frghfqv86>
2021-12-19 02:22:55 +0200

    add hero-1 change titles change buttons change layout, add col

--- stories/Blocks/Heros/Hero-1/Hero.stories.js
+++ stories/Blocks/Heros/Hero-1/Hero.stories.js
@@ -5,22 +5,22 @@ export default {
 };
 
 const Template = () => {
-  return `<section class="hero-1 container">
-    <div class="hero-1__content">
-      <h1 class="hero-1__title title title-h1">Want anything to be easy with LaslesVPN.</h1>
+  return `<section class="hero-1 v-container">
+    <div class="hero-1__content v-col-6">
+      <h1 class="hero-1__title title title-h3">Want anything to be easy with LaslesVPN.</h1>
       <p class="hero-1__text">
         Provide a network for all your needs with ease and fun using LaslesVPN discover interesting features from us.
       </p>
       <img src="${hero_1}"
         alt="Some peoples with phone"
-        class="hero-1__img hero-1__img--mobile"
+        class="hero-1__img hero-1__img--mobile v-col-6"
         loading="lazy"
       >
       <a href="#!" class="hero-1__button button-primary button-primary--medium">Get Started</a>
     </div>
     <img src="${hero_1}"
       alt="Some peoples with phone"
-      class="hero-1__img hero-1__img--desktop"
+      class="hero-1__img hero-1__img--desktop v-col-6"
       loading="lazy"
     >
   </section>`;
