bfce0f8e7b344c0d98ce1274d460e0449a50c644
vadim <frghfqv86>
2022-01-19 00:16:11 +0200

    add Android switcher

---
+++ stories/Components/Switchers/Android/Android.stories.js
@@ -0,0 +1,23 @@
+export default {
+  title: 'Components/Switchers/Android',
+  argTypes: {
+    label: { control: 'text' },
+    checked: { control: 'boolean' },
+    disabled: { control: 'boolean' },
+  }
+};
+
+const Template = ({ label, checked, disabled }) => {
+  return `<label class="android-switch">
+            <input class="android-switch__input" type="checkbox" ${checked ? 'checked' : ''} ${disabled ? 'disabled' : ''}>
+            <span class="android-switch__toggle"></span>
+            <span class="android-switch__label">${label}</span>
+          </label>`;
+};
+
+export const Android = Template.bind({});
+Android.args = {
+  label: 'Android',
+  checked: true,
+  disabled: true
+};
