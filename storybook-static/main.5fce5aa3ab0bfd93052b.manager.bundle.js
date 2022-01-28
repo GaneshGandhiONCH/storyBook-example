67208a8c50af60f3a9d4e892b146523e318a2fb7
vadim <frghfqv86>
2022-01-28 02:50:27 +0200

    add 3 buttons - static

---
+++ storybook-static/main.5fce5aa3ab0bfd93052b.manager.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{690:function(module,exports){},93:function(module,exports,__webpack_require__){__webpack_require__(625),__webpack_require__(388),__webpack_require__(1533),__webpack_require__(1530),module.exports=__webpack_require__(1011)}},[[93,3,4]]]);
