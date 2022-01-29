19a6ef6890962da8c5f7866b09c1cf606b6e25f2
vadim <frghfqv86>
2022-01-30 00:06:29 +0200

    add LangSwitcher-v1

---
+++ stories/Components/LangSwitchers/LangSwitcher-v1/LangSwitcher.js
@@ -0,0 +1,53 @@
+export function LangSwitcher() {
+  // VARS
+  const $this = this;
+  const switchers = document.querySelectorAll('[data-switcher]');
+  if (!switchers.length) return;
+  const activeClass = 'lang-switcher-v1__list--open';
+
+  // LISTENERS
+  document.addEventListener('click', handleOnClickDocument, false);
+
+  document.addEventListener('scroll', handleOnScroll, false);
+
+  document.addEventListener('keydown', handleOnKeyDown);
+
+
+  // HANDLERS
+  function handleOnClickDocument(e) {
+      if(!e.target.closest('[data-switcher]')) {
+        $this.closeAll();
+      }
+  }
+
+  function handleOnScroll() {
+    $this.closeAll();
+  }
+
+  function handleOnKeyDown(e) {
+    if (e.key === "Escape") {
+      $this.closeAll();
+    }
+
+    if (e.key === "Tab") {
+      setTimeout(() => {
+        $this.toggleLangSwitcherOnTab();
+      }, 0);
+    }
+  }
+
+
+  // FUNCTIONS & METHODS
+  $this.closeAll = () => {
+    switchers.forEach(switcher => {
+      switcher.querySelector('[data-switcher-list]').classList.remove(activeClass);
+    })
+  }
+
+  $this.toggleLangSwitcherOnTab = () => {
+    const activeElement = document.activeElement;
+    const activeElementContainer = activeElement.closest('[data-switcher-list]');
+
+    activeElementContainer ? activeElementContainer.classList.add(activeClass) : $this.closeAll();
+  }
+}
---
+++ stories/Components/LangSwitchers/LangSwitcher-v1/LangSwitcher.stories.js
@@ -0,0 +1,46 @@
+import {LangSwitcher} from './LangSwitcher';
+
+export default {
+  title: 'Components/LangSwitchers',
+  component: 'Lang_switcher_version_1',
+  argTypes: {
+    current: { control: 'text' },
+    firstLang: { control: 'text' },
+    secondLang: { control: 'text' }
+  },
+};
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  setTimeout(() => {
+    new LangSwitcher();
+  }, 0)
+})
+
+const Template = ({ current, firstLang, secondLang }) => {
+  return `<div class="lang-switcher-v1" data-switcher>
+    <button class="lang-switcher-v1__current" data-switcher-current="header-switcher">
+      <span class="lang-switcher-v1__current-name">${current}</span> 
+      <svg class="lang-switcher-v1__current-icon" width="11" height="6" viewBox="0 0 11 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0.694458 0.5L5.3473 5L10.0002 0.5" />
+      </svg>
+    </button>
+    <ul class="lang-switcher-v1__list" data-switcher-list>
+      <li class="lang-switcher-v1__list-item">
+        <a href="#!" class="lang-switcher-v1__link lang-switcher-v1__link--current">${current}</a>
+      </li>
+      <li class="lang-switcher-v1__list-item">
+        <a href="#!" class="lang-switcher-v1__link">${firstLang}</a>
+      </li>
+      <li class="lang-switcher-v1__list-item">
+        <a href="#!" class="lang-switcher-v1__link">${secondLang}</a>
+      </li>
+    </ul>
+  </div>`;
+};
+
+export const Lang_switcher_version_1 = Template.bind({});
+Lang_switcher_version_1.args = {
+  current: 'ENG',
+  firstLang: 'DT',
+  secondLang: 'FR'
+};
